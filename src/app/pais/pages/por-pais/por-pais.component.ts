89fecd15cb9aef75012aa0b52995e0f4a8e1f930
santiago monsalve <santi.monsalve09@gmail.com>
2021-06-02 19:29:05 -0500

    version 2: barra de sugerencias y mostrar por region

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -4,19 +4,29 @@ import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
-  templateUrl: './por-pais.component.html'
+  templateUrl: './por-pais.component.html',
+  styles:[
+    `
+    li{
+      cursor: pointer;
+    }
+    `
+  ]
 })
 export class PorPaisComponent{
  
   termino: string ="";
   hayError: boolean=false;
   listaPaises: Country[]=[];
-  
+  listaPaisesSugerencias: Country[]=[];
+  mostrarSugerencias: boolean = false;
+
   //inyeccion de dependencia servise
   constructor(private paisService: PaisService){}
 
   buscar(termino: string): void{
 
+    this.mostrarSugerencias= false;
     this.hayError = false;
     this.termino=termino;
     
@@ -27,10 +37,22 @@ export class PorPaisComponent{
         this.hayError = true;
         this.listaPaises = [];
       })
+      
   }
   
   sugerencias(termino: string):void{
+
     this.hayError=false;
-    //TODO crear sugerencias
+    this.termino= termino;
+    this.mostrarSugerencias=true;
+    this.paisService.buscarPais(termino)
+    .subscribe((paises) => {
+      this.listaPaisesSugerencias=paises.splice(0,5);
+    },(error) => {
+      this.listaPaisesSugerencias=[];
+    })
+
   }
+
+
 }
